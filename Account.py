34abe0a065d9628a4ff47028500fb60e1059b5bd
Mugen203 <kwakuaffram@gmail.com>
2019-07-29 14:07:04 +0000

    Updated

--- Account.py
+++ Account.py
@@ -6,44 +6,28 @@ def class Account(object):
     def checkBalance():
         print("Here is your account balance ", self.balance)
 
+            def debit(amount):
+                    try:
+                        if self.balance >= amount:
+                            debit_Balance = self.balance - amount
+                                print("Transaction succesful!")
+                                print("Your new balance is ", debit_Balance)
+
+                        else:
+                            raise Error
+                    except Error:
+                        print("Insufficient funds!")
+
+
+            def credit(amount):
+                credit_Balance = self.balance + amount
+                print("Your new balance is ", credit_Balance)
+
+
 def class SavingAccount(Account):
     def __init__(self, type, owner, accountNo):
         Account.__init__(self, type, owner)
 
-        def debit(amount):
-                try:
-                    if self.balance >= amount:
-                        debit_Balance = self.balance - amount
-                            print("Transaction succesful!")
-                            print("Your new balance is ", debit_Balance)
-
-                    else:
-                        raise Error
-                except Error:
-                    print("Insufficient funds!")
-
-
-        def credit(amount):
-            credit_Balance = self.balance + amount
-            print("Your new balance is ", credit_Balance)
-
 def class CheckingAccount(Account):
     def __init__(self, type, owner, accountNo):
         Account.__init__(self, type, owner)
-
-                def debit():
-                        try:
-                            if self.balance >= amount:
-                                debit_Balance = self.balance - amount
-                                    print("Transaction succesful!")
-                                    print("Your new balance is ", debit_Balance)
-
-                            else:
-                                raise Error
-                        except Error:
-                            print("Insufficient funds!")
-
-
-                def credit():
-                    credit_Balance = self.balance + amount
-                    print("Your new balance is ", credit_Balance)
